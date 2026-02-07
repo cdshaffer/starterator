27ea162df916d461209e95edb85aca1e80768e51
Chris Shaffer <shaffer@wustl.edu>
2026-02-06 18:23:36 -0600

    fix typo

--- starterator/phams.py
+++ starterator/phams.py
@@ -573,7 +573,7 @@ class Pham(object):
             gene_dict = {}
             gene_dict['GeneID'] = gene.gene_id
             gene_dict['Start'] = gene.start if gene.orientation == "R" else gene.start + 1 #switch to 1 based counting
-            gene_dict['Stop'] = gene.stop  if gene.orientation == "F" else gene.start + 1 #switch to 1 based counting
+            gene_dict['Stop'] = gene.stop  if gene.orientation == "F" else gene.stop + 1 #switch to 1 based counting
             gene_dict['Orientation'] = gene.orientation
             gene_dict['AvailableStarts'] = gene.alignment_candidate_start_nums
             gene_dict['AvailableCoord'] = [gene.alignment_index_to_coord(s) for s in gene.alignment_candidate_starts]
